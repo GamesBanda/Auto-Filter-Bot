64583036b9d7f234c38d9bce8ddd22918b900832
GamesBanda <156589537+GamesBanda@users.noreply.github.com>
2024-07-20 16:44:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ else:
     SUPPORT_GROUP = int(SUPPORT_GROUP)
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://mangolv:uBM0qfRO8YqzlqKH@cluster0.vk1kdu4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0') #mongo db url
+DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://Krlove:kumar12321@cluster0.tsl2qwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0') #mongo db url
 if len(DATABASE_URL) == 0:
     print('Error - DATABASE_URL is missing, exiting now')
     exit()
